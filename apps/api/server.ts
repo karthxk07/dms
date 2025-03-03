119359fdd8695613728268d2cee30363b07d6339
karthxk07 <karthikkalyanam07@gmail.com>
2025-03-03 10:59:02 +0530

    cors origin update

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Your Astro app's URL
+    origin: "*", // Your Astro app's URL
     credentials: true,
   })
 );
