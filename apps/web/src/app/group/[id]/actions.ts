bf48eb1d622e2e54100db997e9ee06e2dd84c05b
karthxk07 <karthikkalyanam07@gmail.com>
2025-04-13 09:26:57 +0530

    debug googleAccessToken not received on prod

--- apps/web/src/app/group/[id]/actions.ts
+++ apps/web/src/app/group/[id]/actions.ts
@@ -5,6 +5,7 @@ import { cookies } from 'next/headers'
 export async function get_googleToken() {
   const cookieStore = await cookies()
  
+  console.log(cookieStore.get('google_accessToken'));
   // Get cookie
   return cookieStore.get('google_accessToken')?.value;
 
