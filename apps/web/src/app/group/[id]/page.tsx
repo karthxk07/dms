7d1e618683982c2f0a721d630b1d45d8cb4c281e
karthxk07 <karthikkalyanam07@gmail.com>
2025-04-13 13:57:09 +0530

    null error fix

--- apps/web/src/app/group/[id]/page.tsx
+++ apps/web/src/app/group/[id]/page.tsx
@@ -230,7 +230,7 @@ export default function GroupFilesDashboard () {
                 />
               </div>
 
-              {googleAccessToken=="" && (
+              {googleAccessToken && (
                 <button
                   type="button"
                   onClick={initiateGoogleAuth}
@@ -252,7 +252,7 @@ export default function GroupFilesDashboard () {
                 </button>
                 <button
                   type="submit"
-                  disabled={isUploading || googleAccessToken==""}
+                  disabled={isUploading || googleAccessToken==null}
                   className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
                 >
                   {isUploading ? "Uploading..." : "Upload"}
