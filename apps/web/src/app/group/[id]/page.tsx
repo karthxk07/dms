fecce890335fa03f6197bda95579c01c78c26e5e
karthxk07 <karthikkalyanam07@gmail.com>
2025-04-13 13:50:50 +0530

    error fix

--- apps/web/src/app/group/[id]/page.tsx
+++ apps/web/src/app/group/[id]/page.tsx
@@ -231,7 +231,7 @@ export default function GroupFilesDashboard () {
                 />
               </div>
 
-              {googleAccessToken?.googleAccessToken=="" && (
+              {googleAccessToken=="" && (
                 <button
                   type="button"
                   onClick={initiateGoogleAuth}
@@ -253,7 +253,7 @@ export default function GroupFilesDashboard () {
                 </button>
                 <button
                   type="submit"
-                  disabled={isUploading || googleAccessToken?.googleAccessToken==""}
+                  disabled={isUploading || googleAccessToken==""}
                   className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
                 >
                   {isUploading ? "Uploading..." : "Upload"}
